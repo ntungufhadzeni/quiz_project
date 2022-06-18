7d3cea2ec42c9140ffb620be9f3dacd3f3f732cf
ntungufhadzeni <ntungufhadzenimbudzeno@gmail.com>
2022-06-18 15:52:12 +0200

    remove wsgi

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import json
 import sqlite3 as sql
-from chatbot import chatbot
 from datetime import datetime
 
 from flask import Flask, render_template, request, url_for, session, flash
@@ -10,6 +9,8 @@ from flask_session import Session
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from service import create_bot
+from train import train_with_csv, train_with_corpus
 
 app = Flask(__name__)
 app.static_folder = 'static'
@@ -17,6 +18,12 @@ app.secret_key = b'_5#y2L"FFF4Q8z\n\xec]/'
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
+# create the bot
+chatbot = create_bot()
+
+# train
+train_with_corpus(chatbot, 'chatterbot.corpus.english')
+train_with_csv(chatbot, 'data/mental_health_faq.csv')
 
 with open("questions.json", "r") as read_file:
     questions = json.load(read_file)
@@ -229,3 +236,7 @@ def chat():
 def get_bot_response():
     user_text = request.args.get('msg')
     return str(chatbot.get_response(user_text))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- chatbot.py
+++
@@ -1,9 +0,0 @@
-from service import create_bot
-from train import train_with_csv, train_with_corpus
-
-# create the bot
-chatbot = create_bot()
-
-# train
-# train_with_corpus(chatbot, 'chatterbot.corpus.english')
-train_with_csv(chatbot, './data/mental_health_faq.csv')
